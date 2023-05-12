27be3023cbfc22863bb862618a0c24ef5b66cc20
EndWall <43109779+EW-EndWall@users.noreply.github.com>
2023-05-12 15:49:27 +0300

    Add files via upload

--- WordPress-Language-Creator.php
+++ WordPress-Language-Creator.php
@@ -6,15 +6,40 @@ function my_theme_get_translatable_strings($dir)
     $di = new RecursiveDirectoryIterator($dir);
     foreach (new RecursiveIteratorIterator($di) as $filename => $file) {
         if (is_file($filename)) {
+            // * ignore hidden files/directories
+            if (substr($file, 0, 1) === '.' || strpos($filename, '/.') !== false) {
+                continue;
+            }
             $file_contents = file_get_contents($filename);
-            preg_match_all('/__\(([^)]+)\)/', $file_contents, $matches);
-            foreach ($matches[1] as $match) {
-                $strings[] = trim($match, "'\"");
+            preg_match_all('/(?:__|_e|esc_html__|esc_attr__)\(\s*([\'"])(.*?)\1\s*(?:,\s*[\'"](.*?)\3\s*)?\)/', $file_contents, $matches, PREG_SET_ORDER);
+            foreach ($matches as $match) {
+                $strings[] = trim($match[2]);
             }
         }
     }
     return array_unique($strings);
 }
+
+// function my_theme_get_translatable_strings($dir, $exclude_prefix = '__')
+// {
+//     $strings = array();
+//     $di = new RecursiveDirectoryIterator($dir);
+//     foreach (new RecursiveIteratorIterator($di) as $filename => $file) {
+//         if (is_file($filename)) {
+//             $file_contents = file_get_contents($filename);
+//             preg_match_all('/(?:__|_e|esc_html__|esc_attr__)\(\s*([\'"])(.*?)\1\s*(?:,\s*[\'"](.*?)\3\s*)?\)/', $file_contents, $matches, PREG_SET_ORDER);
+//             foreach ($matches as $match) {
+//                 $function_name = substr($match[0], 0, strpos($match[0], '('));
+//                 $prefix = substr($function_name, 0, strpos($function_name, '_'));
+//                 $string = trim($match[2]);
+//                 if ($prefix !== $exclude_prefix && substr($prefix, -1) === '_' && substr($prefix, 0, -1) !== $exclude_prefix && substr($string, 0, 2) !== '__') {
+//                     $strings[] = $string;
+//                 }
+//             }
+//         }
+//     }
+//     return array_unique($strings);
+// }
 
 // * Creating a translation file "pot"
 function my_theme_generate_translation_file($dir, $filename, $domain)
